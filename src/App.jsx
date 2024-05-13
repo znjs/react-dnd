f48245ea5c9c864f7d50220a5af7ecef86c95f60
znjs <jasrikar@gmail.com>
2024-05-14 02:23:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { NavBar } from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export const NavBar = () => {
+  return (
+    <div className="bg-white border-b-[1px] border-gray-600 py-3 px-4">
+      <div className="max-w-7xl mx-auto">
+        <h1>React dnd</h1>
+      </div>
+    </div>
+  );
+};
