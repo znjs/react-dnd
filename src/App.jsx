48864d89b75d67f6438cd758f0534226f561fb24
znjs <jasrikar@gmail.com>
2024-05-14 03:01:16 +0530

    feat: added config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import { NavBar } from "./components/Navbar";
+import { filesConfig } from "./utils/config";
 
 function App() {
+  const [files, setFiles] = useState(filesConfig);
+  console.log(files);
   return (
-    <>
+    <div className="min-h-screen min-w-screen flex flex-col">
       <NavBar />
-    </>
+      <div className="w-full bg-[#ecf0fb] grow">
+        <div className="max-w-7xl w-full mx-auto bg-blue-300 p-2 rounded-md">
+          <h1>test</h1>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ src/utils/config.js
@@ -0,0 +1,62 @@
+export const filesConfig = {
+  folder1: [
+    {
+      id: 1,
+      name: "file1",
+      content: "sample content",
+      date: "2024-05-10T21:14:32.262Z",
+    },
+    {
+      id: 2,
+      name: "file2",
+      content: "sample content",
+      date: "2024-05-10T21:14:32.262Z",
+    },
+    {
+      id: 3,
+      name: "file3",
+      content: "sample content",
+      date: "2024-05-10T21:14:32.262Z",
+    },
+  ],
+  folder2: [
+    {
+      id: 4,
+      name: "file4",
+      content: "sample content",
+      date: "2024-05-10T21:14:32.262Z",
+    },
+    {
+      id: 5,
+      name: "file5",
+      content: "sample content",
+      date: "2024-05-10T21:14:32.262Z",
+    },
+    {
+      id: 6,
+      name: "file6",
+      content: "sample content",
+      date: "2024-05-10T21:14:32.262Z",
+    },
+  ],
+  folder3: [
+    {
+      id: 4,
+      name: "file7",
+      content: "sample content",
+      date: "2024-05-10T21:14:32.262Z",
+    },
+    {
+      id: 5,
+      name: "file8",
+      content: "sample content",
+      date: "2024-05-10T21:14:32.262Z",
+    },
+    {
+      id: 6,
+      name: "file9",
+      content: "sample content",
+      date: "2024-05-10T21:14:32.262Z",
+    },
+  ],
+};
